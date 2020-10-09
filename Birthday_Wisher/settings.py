e7ea1a3421791a71e6a1e8c8d2d9b39d543ffab4
Ashutosh Krishna <ashutoshbritish@gmail.com>
2020-10-09 22:51:45 +0530

    Add Postgresql

--- Birthday_Wisher/settings.py
+++ Birthday_Wisher/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
